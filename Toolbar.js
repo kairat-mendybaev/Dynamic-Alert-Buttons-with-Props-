00524f5cc5bd92b3973bf269608ee1e639302b7d
Kairat Mendybaev <113317646+kairat-mendybaev@users.noreply.github.com>
2025-03-13 23:34:48 -0700

    Update Toolbar.js

--- Toolbar.js
+++ Toolbar.js
@@ -1,1 +1,24 @@
+import React from 'react';
+import AlertButton from './AlertButton';
+
+function Toolbar() {
+    const buttons = [
+        { message: 'Downloading!', children: 'Download File' },
+        { message: 'Sharing!', children: 'Share Document' },
+        { message: 'Uploading!', children: 'Upload Image' },
+        { message: 'Deleting!', children: 'Delete File' },
+        { message: 'Saving!', children: 'Save Changes' },
+        { message: 'Exiting!', children: 'Exit Editor' }
+    ];
+
+    return (
+        <div>
+            {buttons.map((btn, index) => (
+                <AlertButton key={index} message={btn.message} children={btn.children} />
+            ))}
+        </div>
+    );
+}
+
+export default Toolbar;
 
